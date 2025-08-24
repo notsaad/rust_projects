8e96c4319fb5da64f5e5e74da4abf97b3f67008f
notsaad <saadmazharr@gmail.com>
2025-08-23 21:26:48 -0700

    add loop to guessing
    Signed-off-by: notsaad <saadmazharr@gmail.com>

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -7,24 +7,28 @@ fn main() {
     println!("Guess a number!");
     let num = rand::thread_rng().gen_range(1..=100);
     println!("The secret number is: {}", num);
-    println!("Input your guess:");
 
-    // mut means that its a mutable variable, by default in rust
-    // variables are immutable (const)
-    let mut guess = String::new();
+    loop {
+        println!("Input your guess:");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        // mut means that its a mutable variable, by default in rust
+        // variables are immutable (const)
+        let mut guess = String::new();
 
-    // expect is like the failure handling thing
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    println!("you guessed: {}", guess);
+        // expect is like the failure handling thing
+        // this is called shadowing (reuse the guess variable and change it to another type)
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
-    match guess.cmp(&num) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        println!("you guessed: {}", guess);
+
+        match guess.cmp(&num) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => println!("You win!"),
+        }
     }
 }
