935ca4319d18e1353329eaceb2f8b4d18ffd1eae
notsaad <saadmazharr@gmail.com>
2025-08-26 20:43:15 -0700

    conditional assignment
    Signed-off-by: notsaad <saadmazharr@gmail.com>

--- chapter_three/control_flow/src/main.rs
+++ chapter_three/control_flow/src/main.rs
@@ -1,6 +1,8 @@
 fn main() {
     let number: i32 = 5;
     
+    // rust will not convert types though
+    // so doing 'if number' would not compile (unlike javascript or ruby)
     if number < 5 {
         println!("number is less than five");
     } else if number > 5 {
@@ -8,4 +10,8 @@ fn main() {
     } else {
         println!("number is five!");
     }
+    
+    // both results need to be of the same type
+    let conditional_number: i32 = if true { 13 } else { 8 };
+    println!("This is the conditional number: {}", conditional_number);
 }
