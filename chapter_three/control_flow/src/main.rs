90b5fd20370048261fffe4080fd204f84b74af58
notsaad <saadmazharr@gmail.com>
2025-08-26 20:40:58 -0700

    if statements
    Signed-off-by: notsaad <saadmazharr@gmail.com>

---
+++ chapter_three/control_flow/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let number: i32 = 5;
+    
+    if number < 5 {
+        println!("number is less than five");
+    } else if number > 5 {
+        println!("number is greater than five");
+    } else {
+        println!("number is five!");
+    }
+}
